0a0ead53b41c6fe404c8ff6026ea8a6e138f413d
socialismbuilder <46308871+socialismbuilder@users.noreply.github.com>
2024-12-21 15:42:41 +0800

    Add files via upload

--- reg.js
+++ reg.js
@@ -102,11 +102,14 @@ function tex_process(text) {
     .replace(/[\(（][\)）]/g,'(　)')
     .replace(/α/g,'\\alpha')
     .replace(/β/g,'\\beta')
+    .replace(/\\leq/g,'\\leqslant')
+    .replace(/\\geq/g,'\\geqslant')
+    .replace(/，/g,',')
     return text;
 }
 
 function html_text(text) {
     text = text
-    .replace(/ ?_{1,2} ?([0-9]{1,3}) ?_{1,2} ?/g, ' <uline style="border-bottom: 1px solid #000;">　$1　</uline> ' )
+    .replace(/ ?_{1,4} ?([0-9]{1,3}) ?_{1,4} ?/g, ' <uline style="border-bottom: 1px solid #000;">　$1　</uline> ' )
     return text;
 }
